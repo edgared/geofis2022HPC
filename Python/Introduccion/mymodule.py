1f440fefb8674b2981e65f6c3913dde3d970f7cc
Edgar R. Rondán <edgared@gmail.com>
2022-07-20 12:51:36 -0300

    Python Jupyter Notebooks

---
+++ Python/Introduccion/mymodule.py
@@ -0,0 +1,11 @@
+"""
+Este es mi primer módulo en Python.
+Saludando a todos.
+"""
+def saludos(name):
+    """
+    Este es mi primer método en Python.
+    Saludos a todos.
+    """
+    # Este es mi primer módulo en Python.
+    print("Hola estimado, " + name) 
